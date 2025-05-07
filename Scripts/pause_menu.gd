4eac90dd317f692c14ff3a9bcdf0d4300f868e2d
Luke Moyer <DudenessBoy_software@disroot.org>
2025-05-06 20:24:33 -0400

    Added GPL notices

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,3 +1,19 @@
+# Veil of Aether is a FOSS action-adventure RPG
+# Copyright (C) 2025  Luke Moyer
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 extends Control
 
 var _is_paused := false:
