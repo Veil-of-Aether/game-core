a97767098d49c8084bf8dc49662e6ea3bf020be1
Luke Moyer <DudenessBoy_software@disroot.org>
2025-05-21 20:54:29 -0400

    Some more theming changes to the title screen

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,5 +1,13 @@
 extends Control
 
+func _ready() -> void:
+    $Buttons/StartBtn.grab_focus()
+
+func _unhandled_input(event: InputEvent) -> void:
+    # check to see if 
+    if event.is_action_pressed("use_button"):
+        get_viewport().gui_get_focus_owner().emit_signal("pressed")
+
 func _on_start_btn_pressed() -> void:
     get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
