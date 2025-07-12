3d874c4f8629f52dda31c9049659730e806f3bec
Luke Moyer <DudenessBoy_software@disroot.org>
2025-07-12 16:06:16 -0400

    More work on the map screen camera

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -4,8 +4,8 @@ var player
 var player_script
 
 @onready var parent := self.get_parent() # the pause menu
-@onready var player_icon := $PlayerMarker # the icon that will represent the player on the map
-@onready var viewport := $MapScreen
+@onready var player_icon := $SubViewportContainer/SubViewport/Root/PlayerMarker # the icon that will represent the player on the map
+@onready var map_camera := $SubViewportContainer/SubViewport/Root/MapScreen # camera to display to the screen
 
 func _ready() -> void:
     # I know this code is gross, but if it ain't broke don't fix it
@@ -13,7 +13,7 @@ func _ready() -> void:
     var root = self.get_parent().get_parent().get_parent()
     player = root.get_node("Player")
     player_script = player.get_script()
-    viewport.make_current()
+    map_camera.make_current()
 
 func _process(delta: float) -> void:
     # set the player icon's transform components
@@ -22,11 +22,15 @@ func _process(delta: float) -> void:
     var player_x = player.position.x + 1000
     var player_y = player.position.z + 1000 # Z in 3D corresponds to Y in 2D
     player_icon.position = Vector2(player_x, player_y)
-    viewport.position.x = clamp(player_icon.position.x, 0, 2000)
-    viewport.position.y = clamp(player_icon.position.y, 0, 2000)
+    
+    # camera position
+    #var camera_x = clamp(player_icon.position.x, 0, 2000)
+    #var camera_y = clamp(player_icon.position.y, 0, 2000)
+    #map_camera.position = Vector2(camera_x, camera_y)
+    
     # rotation
     var player_rotation_y = player.camera.rotation.y
-    player_icon.rotation = -player_rotation_y  # negated to match 2D clockwise rotation
+    player_icon.rotation = -player_rotation_y # negated to match 2D clockwise rotation
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("open_map"): # if the M key is pressed
